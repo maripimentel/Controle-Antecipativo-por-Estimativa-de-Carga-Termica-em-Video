40ac8949c041ac686f02ea1db182b188ee4f2024
Mariana Pimentel <maripimentelms@gmail.com>
2018-09-25 19:37:59 -0300

    Thread 3

--- Integracao/main.py
+++ Integracao/main.py
@@ -26,11 +26,11 @@ print(TAG+'data/hora: '+ str(time))
 
 settings.init()
 
-t = threading.Thread(name='people_counter', target=PeopleCounter)
+t = threading.Thread(name='people_counter', target=counter)
 t.start()
 # numberPeople = PeopleCounter(0, 0, str(time), SAVE_RESULTS)
 d = threading.Thread(name='logs', target=logs)
-d.start
+d.start()
 
 
 # print(TAG+'Final2: '+str(numberPeople))
@@ -39,5 +39,10 @@ print(TAG+'end')
 
 def logs():
 	print(TAG+'Final: '+str(settings.cntUp-settings.cntDown))
-	input()
+	time.sleep(0.3)
+
+def counter():
+	numberPeople = PeopleCounter(0, 0, str(time), SAVE_RESULTS)
+	
+
 
