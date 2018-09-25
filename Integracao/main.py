9deeefc7927adccf3692d6b4d4ae390293251389
Mariana Pimentel <maripimentelms@gmail.com>
2018-09-25 19:27:30 -0300

    Settings.py 2

--- Integracao/main.py
+++ Integracao/main.py
@@ -26,7 +26,7 @@ print(TAG+'data/hora: '+ str(time))
 settings.init()
 numberPeople = PeopleCounter(0, 0, str(time), SAVE_RESULTS)
 
-print(TAG+'Final1: '+str(cntUp-cntDown))
+print(TAG+'Final1: '+str(settings.cntUp-settings.cntDown))
 print(TAG+'Final2: '+str(numberPeople))
 
 print(TAG+'end')
