faef098a51ba4c9354812905c81daedc968dea62
Mariana Pimentel <maripimentelms@gmail.com>
2018-09-25 20:45:04 -0300

    Ïntegrando SQLite

--- Integracao/main.py
+++ Integracao/main.py
@@ -9,20 +9,29 @@
 # coding=utf-8
 
 from peoplecounterlib import *
+from sqlitelib import *
 import threading
 import settings
 import time
 import datetime
 
-def data():
+def data(database):
 	global runEvent
 	while runEvent.is_set():
-		print(TAG+'Final: '+str(settings.cntUp-settings.cntDown))
-		time.sleep(0.3)
-	
+		numberPeople = settings.cntUp-settings.cntDown
+		print(TAG+'Numero de Pessoas: '+str(numberPeople))
+
+		# Calculates inicial time
+		timestamp = time.time()
+		dateTime = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+
+		(tempMeetingRoom, tempLara, tempExternal, doorSignal, compressorSignal) = (0,0,0,0,0)
+
+		InsertData(database, dateTime, tempMeetingRoom, tempLara, tempExternal, doorSignal, numPeople, compressorSignal)
+		time.sleep(60)
 
 def counter(timeHour, SAVE_RESULTS):
-	numberPeople = PeopleCounter(0, 0, str(timeHour), SAVE_RESULTS)
+	PeopleCounter(0, 0, str(timeHour), SAVE_RESULTS)
 
 SAVE_RESULTS = True
 
@@ -34,6 +43,9 @@ timeHour = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%
 
 print(TAG+'data/hora: '+ str(timeHour))
 
+database = InicializeDatabase(str(timeHour))
+CreateTable(database)
+
 settings.init()
 
 runEvent = threading.Event()
@@ -42,7 +54,7 @@ runEvent.set()
 threadPeopleCounter = threading.Thread(name='people_counter', target=counter, args = (str(timeHour), SAVE_RESULTS))
 threadPeopleCounter.start()
 # numberPeople = PeopleCounter(0, 0, str(time), SAVE_RESULTS)
-threadPeopleData = threading.Thread(name='data', target=data)
+threadPeopleData = threading.Thread(name='data', target=data, args = (database))
 threadPeopleData.start()
 
 try:
@@ -52,7 +64,7 @@ except KeyboardInterrupt:
 	runEvent.clear()
 	threadPeopleCounter.join()
 	threadPeopleData.join()
-
+	ReadTable(database)
 
 # print(TAG+'Final2: '+str(numberPeople))
 
