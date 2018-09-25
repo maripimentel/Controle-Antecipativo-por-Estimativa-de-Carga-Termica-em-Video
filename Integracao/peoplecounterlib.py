2cf1d8b3125f9c21fd57430d1b5d9fe63b51e95b
Mariana Pimentel <maripimentelms@gmail.com>
2018-09-25 19:07:12 -0300

    Camel

--- Integracao/peoplecounterlib.py
+++ Integracao/peoplecounterlib.py
@@ -9,26 +9,24 @@
 
 # coding=utf-8
 
+from cameralib import *
 import numpy as np
 import cv2
 import personlib
 import time
 import sys
-
-from picamera import PiCamera
-from cameralib import *
 import os
 
-def PeopleCounter(cnt_up, cnt_down, name, saveResults):
+def PeopleCounter(cntUp, cntDown, name, saveResults):
     TAG = '(peoplecouterlib) '
 
     if(saveResults):
         # Result's path on Raspberry Pi
         path = '../../Resultados/'+name
-        video_name = name
+        videoName = name
 
         print(TAG+'caminho: '+ str(path))
-        print(TAG+'nome do video: '+ str(video_name))
+        print(TAG+'nome do video: '+ str(videoName))
         
         if not os.path.exists(path):
             os.makedirs(path)
@@ -47,21 +45,21 @@ def PeopleCounter(cnt_up, cnt_down, name, saveResults):
     print(TAG+'threshold:'+str(areaTH))
 
     # Define up and down line
-    line_up = int(2*(h/5))
-    line_down = int(3*(h/5))
+    lineUp = int(2*(h/5))
+    lineDown = int(3*(h/5))
 
     # After this lines memory can be free
-    up_limit = int(1*(h/5))
-    down_limit = int(4*(h/5))
+    upLimit = int(1*(h/5))
+    downLimit = int(4*(h/5))
 
     # Calculate important points
-    print(TAG+"y da linha inferior:"+str(line_down))
-    print(TAG+"y da linha superior:"+str(line_up))
-    line_down_color = (255,0,0)
-    line_up_color = (0,0,255)
+    print(TAG+"y da linha inferior:"+str(lineDown))
+    print(TAG+"y da linha superior:"+str(lineUp))
+    lineDownColor = (255,0,0)
+    lineUpColor = (0,0,255)
 
-    (pt1, pt2, pt3, pt4, pt5, pt6, pt7, pt8) = calculatePoints(w, line_up, line_down, up_limit, down_limit)
-    (pts_L1, pts_L2, pts_L3, pts_L4) = calculateLinePoints(pt1, pt2, pt3, pt4, pt5, pt6, pt7, pt8)
+    (pt1, pt2, pt3, pt4, pt5, pt6, pt7, pt8) = calculatePoints(w, lineUp, lineDown, upLimit, downLimit)
+    (ptsL1, ptsL2, ptsL3, ptsL4) = calculateLinePoints(pt1, pt2, pt3, pt4, pt5, pt6, pt7, pt8)
 
     # Creates the backgroud subtractor
     fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows = True)
@@ -72,12 +70,14 @@ def PeopleCounter(cnt_up, cnt_down, name, saveResults):
     kernelCl = np.ones((11,11),np.uint8)
 
     persons = []
-    max_p_age = 5
+    maxPAge = 5
     pid = 1
 
     cont = 1
     for cap in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     # Para um video continuo camera.capture_continuous(rawCapture, format="bgr", use_video_port=True)
+
+
         
         # Read a frame
         frame = cap.array
@@ -93,13 +93,13 @@ def PeopleCounter(cnt_up, cnt_down, name, saveResults):
         fgmask = fgbg.apply(frame)
         fgmask2 = fgbg.apply(frame)
 
-        saveSubtractorImages(saveResults, path, video_name, cont, frame, fgmask)
+        saveSubtractorImages(saveResults, path, videoName, cont, frame, fgmask)
 
         try:
-            (mask, mask2) = preProcess(fgmask, fgmask2, saveResults, path, video_name, cont, kernelOp, kernelCl)
+            (mask, mask2) = preProcess(fgmask, fgmask2, saveResults, path, videoName, cont, kernelOp, kernelCl)
         except:
-            print (TAG+'para cima: '+str(cnt_up))
-            print (TAG+'para baixo: '+str(cnt_down))
+            print (TAG+'para cima: '+str(cntUp))
+            print (TAG+'para baixo: '+str(cntDown))
             break
         
         #################
@@ -121,9 +121,9 @@ def PeopleCounter(cnt_up, cnt_down, name, saveResults):
                 x,y,w,h = cv2.boundingRect(cnt)
 
                 new = True
-                if cy in range(up_limit,down_limit):
+                if cy in range(upLimit,downLimit):
                     for i in persons:
-                        (i, persons, cnt_up, cnt_down, stopLoop, new) = defineDirection(i, cx, cy, w, h, new, cnt_up, cnt_down, line_up, line_down, up_limit, down_limit, persons, TAG)
+                        (i, persons, cntUp, cntDown, stopLoop, new) = defineDirection(i, cx, cy, w, h, new, cntUp, cntDown, lineUp, lineDown, upLimit, downLimit, persons, TAG)
                         if i.timedOut():
                             # If it reaches timeout, remove person from list
                             index = persons.index(i)
@@ -133,16 +133,16 @@ def PeopleCounter(cnt_up, cnt_down, name, saveResults):
                             break
 
                     if new == True:
-                        p = personlib.MyPerson(pid,cx,cy, max_p_age)
+                        p = personlib.MyPerson(pid,cx,cy, maxPAge)
                         persons.append(p)
                         pid += 1
                         
                 
                 # Drawing persons
-                drawPersons(frame, cx, cy, x, y, w, h, saveResults, path, video_name, cont, cnt)
+                drawPersons(frame, cx, cy, x, y, w, h, saveResults, path, videoName, cont, cnt)
                 
         # Drawing tracking
-        drawTrack(frame, persons, cnt_up, cnt_down, line_down_color, line_up_color, pts_L1, pts_L2, pts_L3, pts_L4, saveResults, path, video_name, cont)
+        drawTrack(frame, persons, cntUp, cntDown, lineDownColor, lineUpColor, ptsL1, ptsL2, ptsL3, ptsL4, saveResults, path, videoName, cont)
         
         cont+=1 
         
@@ -159,39 +159,39 @@ def PeopleCounter(cnt_up, cnt_down, name, saveResults):
     
     cv2.destroyAllWindows()
 
-    return cnt_up - cnt_down
+    return cntUp - cntDown
 
-def calculatePoints(w, line_up, line_down, up_limit, down_limit):
-    pt1 =  [0, line_down];
-    pt2 =  [w, line_down];
+def calculatePoints(w, lineUp, lineDown, upLimit, downLimit):
+    pt1 =  [0, lineDown];
+    pt2 =  [w, lineDown];
 
-    pt3 =  [0, line_up];
-    pt4 =  [w, line_up];
+    pt3 =  [0, lineUp];
+    pt4 =  [w, lineUp];
 
-    pt5 =  [0, up_limit];
-    pt6 =  [w, up_limit];
+    pt5 =  [0, upLimit];
+    pt6 =  [w, upLimit];
     
-    pt7 =  [0, down_limit];
-    pt8 =  [w, down_limit];
+    pt7 =  [0, downLimit];
+    pt8 =  [w, downLimit];
 
     return (pt1, pt2, pt3, pt4, pt5, pt6, pt7, pt8)
 
 def calculateLinePoints(pt1, pt2, pt3, pt4, pt5, pt6, pt7, pt8):
-    pts_L1 = np.array([pt1,pt2], np.int32)
-    pts_L1 = pts_L1.reshape((-1,1,2))
+    ptsL1 = np.array([pt1,pt2], np.int32)
+    ptsL1 = ptsL1.reshape((-1,1,2))
 
-    pts_L2 = np.array([pt3,pt4], np.int32)
-    pts_L2 = pts_L2.reshape((-1,1,2))
+    ptsL2 = np.array([pt3,pt4], np.int32)
+    ptsL2 = ptsL2.reshape((-1,1,2))
 
-    pts_L3 = np.array([pt5,pt6], np.int32)
-    pts_L3 = pts_L3.reshape((-1,1,2))
+    ptsL3 = np.array([pt5,pt6], np.int32)
+    ptsL3 = ptsL3.reshape((-1,1,2))
 
-    pts_L4 = np.array([pt7,pt8], np.int32)
-    pts_L4 = pts_L4.reshape((-1,1,2))
+    ptsL4 = np.array([pt7,pt8], np.int32)
+    ptsL4 = ptsL4.reshape((-1,1,2))
 
-    return (pts_L1, pts_L2, pts_L3, pts_L4)
+    return (ptsL1, ptsL2, ptsL3, ptsL4)
 
-def drawTrack(frame, persons, cnt_up, cnt_down, line_down_color, line_up_color, pts_L1, pts_L2, pts_L3, pts_L4, saveResults, path, video_name, cont):
+def drawTrack(frame, persons, cntUp, cntDown, lineDownColor, lineUpColor, ptsL1, ptsL2, ptsL3, ptsL4, saveResults, path, videoName, cont):
     font = cv2.FONT_HERSHEY_SIMPLEX
 
     for i in persons:
@@ -200,45 +200,45 @@ def drawTrack(frame, persons, cnt_up, cnt_down, line_down_color, line_up_color, 
     #################
     #     IMAGES    #
     #################
-    str_up = 'UP: '+ str(cnt_up)
-    str_down = 'DOWN: '+ str(cnt_down)
-    frame = cv2.polylines(frame,[pts_L1],False,line_down_color,thickness=2)
-    frame = cv2.polylines(frame,[pts_L2],False,line_up_color,thickness=2)
-    frame = cv2.polylines(frame,[pts_L3],False,(255,255,255),thickness=1)
-    frame = cv2.polylines(frame,[pts_L4],False,(255,255,255),thickness=1)
-    cv2.putText(frame, str_up ,(10,40),font,0.5,(255,255,255),2,cv2.LINE_AA)
-    cv2.putText(frame, str_up ,(10,40),font,0.5,(0,0,255),1,cv2.LINE_AA)
-    cv2.putText(frame, str_down ,(10,90),font,0.5,(255,255,255),2,cv2.LINE_AA)
-    cv2.putText(frame, str_down ,(10,90),font,0.5,(255,0,0),1,cv2.LINE_AA)
+    strUp = 'UP: '+ str(cntUp)
+    strDown = 'DOWN: '+ str(cntDown)
+    frame = cv2.polylines(frame,[ptsL1],False,lineDownColor,thickness=2)
+    frame = cv2.polylines(frame,[ptsL2],False,lineUpColor,thickness=2)
+    frame = cv2.polylines(frame,[ptsL3],False,(255,255,255),thickness=1)
+    frame = cv2.polylines(frame,[ptsL4],False,(255,255,255),thickness=1)
+    cv2.putText(frame, strUp ,(10,40),font,0.5,(255,255,255),2,cv2.LINE_AA)
+    cv2.putText(frame, strUp ,(10,40),font,0.5,(0,0,255),1,cv2.LINE_AA)
+    cv2.putText(frame, strDown ,(10,90),font,0.5,(255,255,255),2,cv2.LINE_AA)
+    cv2.putText(frame, strDown ,(10,90),font,0.5,(255,0,0),1,cv2.LINE_AA)
 
     cv2.imshow('Frame',frame)
 
     if (saveResults):
-        name_img_final = path + '/' + video_name + '_' + str(cont) + '_final.jpg'
-        cv2.imwrite(name_img_final,frame)
+        nameImgFinal = path + '/' + videoName + '_' + str(cont) + '_final.jpg'
+        cv2.imwrite(nameImgFinal,frame)
 
-def drawPersons(frame, cx, cy, x, y, w, h, saveResults, path, video_name, cont, cnt):
+def drawPersons(frame, cx, cy, x, y, w, h, saveResults, path, videoName, cont, cnt):
     cv2.circle(frame,(cx,cy), 5, (0,0,255), -1)
     img = cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
 
     if (saveResults):
-        name_img_rectangle = path + '/' + video_name + '_' + str(cont) + '_rectangle.jpg'
-        cv2.imwrite(name_img_rectangle,img)
+        nameImgRectangle = path + '/' + videoName + '_' + str(cont) + '_rectangle.jpg'
+        cv2.imwrite(nameImgRectangle,img)
     
-    img_contours = cv2.drawContours(frame, cnt, -1, (0,255,0), 3)
+    imgContours = cv2.drawContours(frame, cnt, -1, (0,255,0), 3)
 
     if (saveResults):
-        name_img_cont = path + '/' + video_name + '_' + str(cont) + '_contours.jpg'
-        cv2.imwrite(name_img_cont,img_contours)
+        nameImgCont = path + '/' + videoName + '_' + str(cont) + '_contours.jpg'
+        cv2.imwrite(nameImgCont,imgContours)
 
-def saveSubtractorImages(saveResults, path, video_name, cont, frame, fgmask):
+def saveSubtractorImages(saveResults, path, videoName, cont, frame, fgmask):
     if(saveResults):
-        name_img_original = path + '/' + video_name + '_' + str(cont) + '_original.jpg'
-        name_img_sub = path + '/' + video_name + '_' + str(cont) + '_subtractor.jpg'
-        cv2.imwrite(name_img_original,frame)
-        cv2.imwrite(name_img_sub,fgmask)
+        nameImgOriginal = path + '/' + videoName + '_' + str(cont) + '_original.jpg'
+        nameImgSub = path + '/' + videoName + '_' + str(cont) + '_subtractor.jpg'
+        cv2.imwrite(nameImgOriginal,frame)
+        cv2.imwrite(nameImgSub,fgmask)
 
-def preProcess(fgmask, fgmask2, saveResults, path, video_name, cont, kernelOp, kernelCl):
+def preProcess(fgmask, fgmask2, saveResults, path, videoName, cont, kernelOp, kernelCl):
 
     # Eliminate shadows
     ret,imBin= cv2.threshold(fgmask,200,255,cv2.THRESH_BINARY)
@@ -253,32 +253,30 @@ def preProcess(fgmask, fgmask2, saveResults, path, video_name, cont, kernelOp, k
     mask2 = cv2.morphologyEx(mask2, cv2.MORPH_CLOSE, kernelCl)
     
     if(saveResults):
-        name_img_transf = path + '/' + video_name + '_' + str(cont) + '_transformation.jpg'
-        cv2.imwrite(name_img_transf,mask)
+        nameImgTransf = path + '/' + videoName + '_' + str(cont) + '_transformation.jpg'
+        cv2.imwrite(nameImgTransf,mask)
 
     return (mask, mask2)
 
-def  defineDirection(i, cx, cy, w, h, new, cnt_up, cnt_down, line_up, line_down, up_limit, down_limit, persons, TAG):
+def  defineDirection(i, cx, cy, w, h, new, cntUp, cntDown, lineUp, lineDown, upLimit, downLimit, persons, TAG):
     stopLoop = False
-
-    print ('I: '+str(i))
 
     if abs(cx-i.getX()) <= w and abs(cy-i.getY()) <= h:
         # Close to a person already detected
         new = False
         i.updateCoords(cx,cy)   #Refresh
-        if i.going_UP(line_down,line_up) == True:
-            cnt_up += 1;
+        if i.going_UP(lineDown,lineUp) == True:
+            cntUp += 1;
             print(TAG+str(i.getId()) +' foi para cima aos '+time.strftime("%c"))
-        elif i.going_DOWN(line_down,line_up) == True:
-            cnt_down += 1;
+        elif i.going_DOWN(lineDown,lineUp) == True:
+            cntDown += 1;
             print(TAG+str(i.getId()) +' foi para baixo aos '+time.strftime("%c"))
         stopLoop = True
     if i.getState() == '1':
-        if i.getDir() == 'down' and i.getY() > down_limit:
+        if i.getDir() == 'down' and i.getY() > downLimit:
             i.setDone()
-        elif i.getDir() == 'up' and i.getY() < up_limit:
+        elif i.getDir() == 'up' and i.getY() < upLimit:
             i.setDone()  
 
-    return (i, persons, cnt_up, cnt_down, stopLoop, new)
+    return (i, persons, cntUp, cntDown, stopLoop, new)
 
