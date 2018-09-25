d7aba31b36294886095025b057ac3c36ac5b1a67
maripimentel <maripimentelms@gmail.com>
2018-09-25 20:14:05 -0300

    identacao

--- Integracao/main.py
+++ Integracao/main.py
@@ -49,8 +49,8 @@ try:
 		time.sleep(0.3)
 except KeyboardInterrupt:
 	runEvent.clear()
-    threadPeopleCounter.join()
-    threadPeopleData.join()
+	threadPeopleCounter.join()
+	threadPeopleData.join()
 
 
 # print(TAG+'Final2: '+str(numberPeople))
