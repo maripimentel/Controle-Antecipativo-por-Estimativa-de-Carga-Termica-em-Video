f96f2f198a21a4290bf654fd4001217d422a96a2
maripimentel <maripimentelms@gmail.com>
2018-11-02 17:43:08 -0300

    Novo video

--- Integracao/makemove.py
+++ Integracao/makemove.py
@@ -5,7 +5,13 @@ name = '2018-11-01 15:57:16'
 image_folder = '../../Resultados/'+name
 video_name = 'Log/' + name + '.avi'
 
-images = [img for img in os.listdir(image_folder) if img.endswith("final.jpg")]
+images=[]
+for cont in range(1, 567):
+    for img in os.listdir(image_folder):
+        if img.endswith(str(cont)+"_final.jpg"):
+            images.append(img)
+            
+#images = [img for img in os.listdir(image_folder) if img.endswith("final.jpg")]
 frame = cv2.imread(os.path.join(image_folder, images[0]))
 height, width, layers = frame.shape
 
