32e6f8a0579a74f66e733be75eb4270edc7bf6c5
maripimentel <maripimentelms@gmail.com>
2018-11-10 10:12:49 -0200

    teste PI com perturbacao final 2

--- Integracao/peoplecounterlib.py
+++ Integracao/peoplecounterlib.py
@@ -70,7 +70,7 @@ def PeopleCounter(cntUp, cntDown, name, saveResults):
     (ptsL1, ptsL2, ptsL3, ptsL4) = calculateLinePoints(pt1, pt2, pt3, pt4, pt5, pt6, pt7, pt8)
 
     # Creates the backgroud subtractor
-    fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows = True)
+    fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows = False)
 
     # Necessary to apply filters and morfological transforms
     kernelOp = np.ones((5,5),np.uint8)
